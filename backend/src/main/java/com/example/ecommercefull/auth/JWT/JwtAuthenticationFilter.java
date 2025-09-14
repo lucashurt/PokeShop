a7aebf3cac969fb1c4196069e859cefd3e17aa48
lucashurt <lucashurtado814@gmail.com>
2025-09-14 10:47:16 -0400

    created JWT auth filter and util service for jwt tokens

---
+++ backend/src/main/java/com/example/ecommercefull/auth/JWT/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.example.ecommercefull.auth.JWT;
+
+import com.example.ecommercefull.auth.models.User;
+import com.example.ecommercefull.auth.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, UserRepository userRepository) {
+        this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException{
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String jwt = authHeader.substring(7);
+
+        try{
+            if(jwtUtil.isValid(jwt)){
+                String username = jwtUtil.getUsernameFromToken(jwt);
+                String role = jwtUtil.getRole(jwt);
+
+                Optional <User> user = userRepository.findByUsername(username);
+                if(user.isPresent()){
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            user,
+                            null,
+                            List.of(new SimpleGrantedAuthority(role))
+                    );
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+        }catch (Exception e){
+            logger.error("JWT Authentication Error");
+        }
+        filterChain.doFilter(request, response);
+    }
+}
